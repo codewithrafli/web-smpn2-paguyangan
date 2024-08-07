60c98ab28c1e256f6b549d17ab4eeabc1d406526
xrafffcode <yusfiwawan2@gmail.com>
2024-08-07 07:34:49 +0700

    up

--- resources/views/components/frontend/navbar.blade.php
+++ resources/views/components/frontend/navbar.blade.php
@@ -71,22 +71,22 @@
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end border-0 shadow-sm p-1" role="menu">
                     <li>
-                        <a class="dropdown-item rounded-1" href="bintangpusnas.perpusnas.go.id">
+                        <a class="dropdown-item rounded-1" href="https://bintangpusnas.perpusnas.go.id">
                             bintangpusnas.perpusnas.go.id
                         </a>
-                        <a class="dropdown-item rounded-1 mt-1" href="e-resources.perpusnas.go.id">
+                        <a class="dropdown-item rounded-1 mt-1" href="https://e-resources.perpusnas.go.id">
                             e-resources.perpusnas.go.id
                         </a>
-                        <a class="dropdown-item rounded-1 mt-1" href="ipunas.id">
+                        <a class="dropdown-item rounded-1 mt-1" href="https://ipunas.id">
                             ipunas.id
                         </a>
-                        <a class="dropdown-item rounded-1 mt-1" href="buku.kemdikbud.go.id">
+                        <a class="dropdown-item rounded-1 mt-1" href="https://buku.kemdikbud.go.id">
                             buku.kemdikbud.go.id
                         </a>
-                        <a class="dropdown-item rounded-1 mt-1" href="budi.kemdikbud.go.id">
+                        <a class="dropdown-item rounded-1 mt-1" href="https://budi.kemdikbud.go.id">
                             budi.kemdikbud.go.id
                         </a>
-                        <a class="dropdown-item rounded-1 mt-1" href="perpustakaan.kemdikbud.go.id">
+                        <a class="dropdown-item rounded-1 mt-1" href="https://perpustakaan.kemdikbud.go.id">
                             perpustakaan.kemdikbud.go.id
                         </a>
                     </li>
